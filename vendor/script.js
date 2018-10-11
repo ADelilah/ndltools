fda09e0df5223397d8f4a27eac565d9a1e82dac2
Anastasia Voronenko <aciadelilah@Anastasias-MacBook-Pro.local>
2018-10-11 20:26:59 +0300

    homepage draft and stuff

---
+++ vendor/script.js
@@ -0,0 +1,9 @@
+$("#slider").on('slide.bs.carousel', function(evt) {
+
+	   var step = $(evt.relatedTarget).index();
+
+	   $('#slider_captions .carousel-caption:not(#caption-'+step+')').fadeOut('fast', function() {
+	   		$('#caption-'+step).fadeIn();
+	   });
+
+	});
